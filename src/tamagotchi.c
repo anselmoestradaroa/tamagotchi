9c5bd5d8d5aab69c960a27b3da8969816d07f273
Anselmo Estrada Roa <anselmoestradaroa@gmail.com>
2021-12-31 02:33:46 -0300

    fff

--- src/tamagotchi.c
+++ src/tamagotchi.c
@@ -14,6 +14,6 @@
 int main(void) {
 	printf("Hola mundo,\n");
 	printf("Ahora pude conectar Git con eclipse, jajaja");
-	printf("Ahora voy a hac er un push al repositorio");
+	printf("Ahora vdoy a hac er un push al repositorio");
 	return 0;
 }
